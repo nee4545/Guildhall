d0f173c88e830c71385e1e5f62e07f6a814fe2aa
Jairam, Neeraj <njairam@mail.smu.edu>
2020-03-09 14:26:56 -0500

    some updates

--- Engine/Code/Engine/Renderer/RenderBuffer.cpp
+++ Engine/Code/Engine/Renderer/RenderBuffer.cpp
@@ -161,6 +161,9 @@ bool RenderBuffer::Create( size_t dataByteSize , size_t elementByteSize )
 	desc.MiscFlags =0;
 	desc.StructureByteStride = (unsigned int)elementByteSize;
 
+	m_bufferByteSize = dataByteSize;
+	m_elementByteSize = elementByteSize;
+
 	device->CreateBuffer( &desc , nullptr , &m_handle );
 
 	return( m_handle != nullptr );
