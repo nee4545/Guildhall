b37d2c35293f9ec0fb1b87d9361f3b443a2de605
Jairam, Neeraj <njairam@mail.smu.edu>
2020-06-02 05:14:06 -0500

    some updates

--- Engine/Code/Engine/Core/EventSystem.cpp
+++ Engine/Code/Engine/Core/EventSystem.cpp
@@ -10,7 +10,7 @@ void EventSystem::ShutDown()
 
 }
 
-void EventSystem::SubscribeToEvent( const std::string& eventName, EventCallBackFunction eventToListen )
+void EventSystem::SubscribeToEvent( const std::string& eventName , EventCallBackFunction eventToListen )
 {
 	EventSubscription* newSubscription = new EventSubscription();
 	newSubscription->m_eventName = eventName;
@@ -18,9 +18,10 @@ void EventSystem::SubscribeToEvent( const std::string& eventName, EventCallBackF
 	m_subscriptions.push_back( newSubscription );
 }
 
-void EventSystem::FireEvent( const std::string& eventName, EventArgs& args )
+
+void EventSystem::FireEvent( const std::string& eventName , EventArgs& args )
 {
-	for( int eventIndex = 0; eventIndex < (int)m_subscriptions.size(); eventIndex++ )
+	for ( int eventIndex = 0; eventIndex < ( int ) m_subscriptions.size(); eventIndex++ )
 	{
 		if ( m_subscriptions[ eventIndex ] != nullptr )
 		{
@@ -31,23 +32,35 @@ void EventSystem::FireEvent( const std::string& eventName, EventArgs& args )
 			}
 		}
 	}
+
+	for ( int i = 0; i < ( int ) m_methodSubscriptions.size(); i++ )
+	{
+		if ( m_methodSubscriptions[ i ] != nullptr )
+		{
+			if ( m_methodSubscriptions[ i ]->eventName == eventName )
+			{
+				m_methodSubscriptions[ i ]->func( args );
+			}
+		}
+	}
 }
 
-void EventSystem::UnSubscribeToEvent( const std::string& eventName, EventCallBackFunction eventToUnscribe )
+void EventSystem::UnSubscribeToEvent( const std::string& eventName , EventCallBackFunction eventToUnscribe )
 {
-	for( int eventIndex =0; eventIndex< m_subscriptions.size(); eventIndex++ )
+	for ( int eventIndex = 0; eventIndex < m_subscriptions.size(); eventIndex++ )
 	{
-		if(m_subscriptions[eventIndex]!=nullptr )
-		{ 
-			if( m_subscriptions[eventIndex]->m_eventName == eventName )
+		if ( m_subscriptions[ eventIndex ] != nullptr )
+		{
+			if ( m_subscriptions[ eventIndex ]->m_eventName == eventName )
 			{
-				if( m_subscriptions[eventIndex]->m_eventToListen == eventToUnscribe )
+				if ( m_subscriptions[ eventIndex ]->m_eventToListen == eventToUnscribe )
 				{
-				delete m_subscriptions[eventIndex];
-				m_subscriptions[eventIndex]=nullptr;
+					delete m_subscriptions[ eventIndex ];
+					m_subscriptions[ eventIndex ] = nullptr;
 				}
 			}
 		}
 	}
+
 }
 
